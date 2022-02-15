3046adf28e493b677a2dfd647969524e58c8e490
sajjad heydari <sajjadheydari1401@gmail.com>
2022-02-15 17:10:01 +0330

    Sample SmartContract Deployed

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+const Marketplace = artifacts.require("Marketplace");
+
+module.exports = function(deployer) {
+  deployer.deploy(Marketplace);
+};
---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.16;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "Simon Marketplace";
+    }
+}
